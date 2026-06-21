6b27b9acb1a36ee0a80294b270c9fcc161f86dde
jecp1897 <jhoncastropatino@gmail.com>
2026-06-21 09:42:02 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,8 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_USER
   }
 });
 
