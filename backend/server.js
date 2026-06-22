3863d5af8faf5a684c13987f9e2f4d8434a794b6
jecp1897 <jhoncastropatino@gmail.com>
2026-06-22 19:15:08 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,13 +24,14 @@ app.use(express.json());
 // CONFIGURAR NODEMAILER
 // -------------------------
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
 });
-
 // -------------------------
 // RUTA DE PRUEBA
 // -------------------------
